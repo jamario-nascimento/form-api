d9a98364142484c1686b9f61174c2f42c58a54ff
Jamario NAscimento <jamariobatista@gmail.com>
2023-07-13 20:40:51 -0300

    Rotas ajustadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BeneficioController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'beneficio');
 
-Route::post('/enviar', 'BeneficioController@enviarParaAPI')->name('enviar');
+Route::get("/enviar", [BeneficioController::class, "enviarParaAPI"]);
